660a9edf9c8f7b279f06d816eae022cc8e44e915
Carlos Flores <cafg10@gmail.com>
2016-04-06 11:50:17 -0600

    Use authorization

--- updater/views.py
+++ updater/views.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
+from bridge.models import Banco
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.urlresolvers import reverse
 from django.shortcuts import get_object_or_404
 from django.views.generic import DetailView, ListView
@@ -10,16 +13,19 @@ from django.utils.translation import ugettext_lazy as _
 from updater.models import BankUpdateFile
 
 
-class BankUpdateFileDetailView(DetailView):
+class BankUpdateFileDetailView(LoginRequiredMixin, DetailView):
     model = BankUpdateFile
 
 
-class BankUpdateFileListView(ListView):
+class BankUpdateFileListView(LoginRequiredMixin, ListView):
+    """
+    Shows the List of :class:`BankUpdateFiles that have not been processed yet.
+    """
     model = BankUpdateFile
     queryset = BankUpdateFile.objects.filter(procesado=False)
 
 
-class BankUpdateFileProcess(RedirectView):
+class BankUpdateFileProcess(LoginRequiredMixin, RedirectView):
     permanent = False
 
     def get_redirect_url(self, *args, **kwargs):
@@ -32,3 +38,10 @@ class BankUpdateFileProcess(RedirectView):
         )
 
         return reverse('bank-update-file-index')
+
+
+class BancoListView(LoginRequiredMixin, ListView):
+    """
+    Shows the UI to handle the :class:`Banco`
+    """
+    model = Banco
